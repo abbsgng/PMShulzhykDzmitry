d4b8746d84b66104e1d4a87a6ec8b93838b96e7b
abbsgng <55626171+abbsgng@users.noreply.github.com>
2019-12-10 16:42:50 +0300

    Update workWithString.h

--- 1course1semester/Headers/workWithString.h
+++ 1course1semester/Headers/workWithString.h
@@ -15,6 +15,20 @@ int getlength(const char* source)
 	}
 	return length;
 }
+int getlengthWord(const char* source)
+{
+	if (source == nullptr)
+	{
+		throw std::invalid_argument("String cannot be null!");
+	}
+
+	int length = 0;
+	while (source[length] && source[length] != ' ')
+	{
+		length++;
+	}
+	return length;
+}
 char* createString(const char* source)
 {
 	//validation
@@ -45,33 +59,30 @@ char toUpper(char symbol)
 	}
 	return symbol;
 }
-int comapre(const char* lhs,int const lhsLength,  const char* rhs, int const rhsLength, bool ordinal)
+bool compare(const char* lhs, int const lhsLength, const char* rhs, int const rhsLength, bool ordinal)
 {
 	//validation
-
+	bool t = true;
 	for (int i = 0; i <= lhsLength || i <= rhsLength; i++)
 	{
 		char l = ordinal ? lhs[i] : toUpper(lhs[i]);
 		char r = ordinal ? rhs[i] : toUpper(rhs[i]);
-
-		if (l = r)
+		if (l == r)
 		{
+			t = true;
 			continue;
 		}
 		if (l > r)
 		{
-			return 1;
+			return true;
 		}
-		else
+		if (l < r)
 		{
-			return -1;
+			return false;
 		}
 
 	}
-	if (lhsLength == rhsLength)
-	{
-		return 0;
-	}
+	return true;
 
 }
 void validation(char* string, int lengthString, char* subString, int lengthSubString)
@@ -128,6 +139,40 @@ int indexSubstringInString(char* string, int lengthString, char* subString, int 
 		}
 	}
 	return index;
+}
+void copyString(char* const target, const char* source)
+{
+	if (target == nullptr)
+	{
+		throw std::invalid_argument("Target is null!");
+	}
+
+	if (source == nullptr)
+	{
+		throw std::invalid_argument("Source is null!");
+	}
+
+	int targetLength = getlength(target);
+
+	int sourceLength = getlength(source);
+
+	if (targetLength < sourceLength)
+	{
+		throw std::invalid_argument("Length destination string!");
+	}
+
+	int i = 0;
+	for (int i = 1; i < 256; i++)
+	{
+		target[i] = '\0';
+	}
+	while (source[i])
+	{
+		target[i] = source[i];
+		i++;
+	}
+	target[i] = '\0';
+
 }
 
 
