5eab3ad131190cba6fbf612ef2abcb781c319090
abbsgng <55626171+abbsgng@users.noreply.github.com>
2020-02-15 18:43:37 +0300

    Update Source.cpp

--- 1course2semester/12.02.2020/Source.cpp
+++ 1course2semester/12.02.2020/Source.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+void menu();
+void console();
+void runTest();
+void testing(short);
+bool arrayIsSorted(Point*, const int);
+void shuffle(Point*, const int);
 void generateArrayPointers(Point*, const int);
 void displayArrayPointers(Point*, const int);
 double* createArrayLengths(Point*, const int);
@@ -18,6 +24,42 @@ void deleteMemory(double*);
 
 int main()
 {
+	menu();
+	return 0;
+}
+
+void menu()
+{
+	while (true)
+	{
+		cout << "Press 1 to work with console.\n";
+		cout << "Press 2 to start test.\n";
+		cout << "Press 3 to exit.\n";
+		char n;
+		cin >> n;
+		system("cls");
+		switch (n)
+		{
+		case '1':
+			console();
+			system("pause");
+			break;
+		case '2':
+			runTest();
+			system("pause");
+			break;
+		case '3':
+			return;
+		default:
+			cout << "Error. Try again.\n";
+			system("pause");
+			system("cls");
+		}
+		system("cls");
+	}
+}
+void console()
+{
 	srand(time(0));
 	const int n = 10;
 	Point* pointers = new Point[n];
@@ -29,9 +71,7 @@ int main()
 	displayArrayPointers(pointers, n);
 	deleteMemory(pointers);
 	deleteMemory(lengths);
-	return 0;
 }
-
 
 void swap(Point* a, Point* b)
 {
@@ -115,4 +155,58 @@ void deleteMemory(Point* pointers)
 void deleteMemory(double* lengths)
 {
 	delete[] lengths;
+}
+
+void runTest()
+{
+	testing(1);
+	testing(2);
+	testing(3);
+	testing(4);
+	testing(5);
+}
+void testing(short number)
+{
+	const int n = number * 1000;
+	Point* pointers = new Point[n];
+	generateArrayPointers(pointers, n);
+	bubleSortPointers(pointers, n);
+	if (arrayIsSorted(pointers, n))
+	{
+		cout << "Test #" << number << "(a) is correct." << endl;
+	}
+	else
+	{
+		cout << "Test #" << number << "(a) isn't correct." << endl;
+	}
+	shuffle(pointers, n);
+	bubleSortPointers(pointers, n);
+	if (arrayIsSorted(pointers, n))
+	{
+		cout << "Test #" << number << "(b) is correct." << endl;
+	}
+	else
+	{
+		cout << "Test #" << number << "(b) isn't correct." << endl;
+	}
+	deleteMemory(pointers);
+}
+
+bool arrayIsSorted(Point* pointers, const int n)
+{
+	for (int i = 0; i < n - 1; ++i)
+	{
+		if (pointers[i].lengthVector() > pointers[i + 1].lengthVector())
+		{
+			return false;
+		}
+	}
+	return true;
+}
+void shuffle(Point* pointers, const int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		swap(pointers[rand() % n], pointers[rand() % n]);
+	}
 }
