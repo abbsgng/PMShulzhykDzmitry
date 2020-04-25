9bbe442b858c1b080fd89968c5a48412e2d2ba2d
abbsgng <55626171+abbsgng@users.noreply.github.com>
2020-04-25 22:00:08 +0300

    Update Source.cpp

--- 1course2semester/15.04.2020/Source.cpp
+++ 1course2semester/15.04.2020/Source.cpp
@@ -8,14 +8,9 @@ using namespace std;
 
 int main()
 {
-	char temp1[1000] = "154567345";
-	char temp2[1000] = "56784356";
-	BigInteger number1(temp1);
-	BigInteger number2(temp2);
-	BigInteger number3;
-	number3 = number1-number2;
-	number3.display();
-	return 0;
+	const char* number1 = "14253647";
+	BigInteger number(number1);
+	cout << number;
 }
 
 
