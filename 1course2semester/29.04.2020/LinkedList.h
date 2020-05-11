ff207b2e7a6952e00219423f4d9fdf4846790fde
abbsgng <55626171+abbsgng@users.noreply.github.com>
2020-05-11 12:10:34 +0300

    Update LinkedList.h

--- 1course2semester/29.04.2020/LinkedList.h
+++ 1course2semester/29.04.2020/LinkedList.h
@@ -9,17 +9,23 @@ class LinkedList
 public:
 	LinkedList() : head(nullptr) {}
 	LinkedList(int*, int);
+	LinkedList(const LinkedList&);
 	~LinkedList();
-	bool isEmpty();
+	
+	bool haveElement(int) const;
+	int size() const;
 	void addFirst(int);
 	void addBeforeItem(int, int);
 	void addAfterItem(int, int);
 	void addLast(int);
 	void removeElement(int);
 	void clear();
-	int* copyToArray(int);
+	int* copyToArray(int) const;
+
 	friend std::ostream& operator << (ostream&, const LinkedList&);
 private:
+	inline bool isEmpty() const;
+
 	struct Node
 	{
 		Node() : next(nullptr) { }
